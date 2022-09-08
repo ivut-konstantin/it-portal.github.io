70b2619dad63ae24f0ecb139bc1e4009dd7ff653
Konstantin Ivut <konstantin.ivut@mail.ru>
2022-09-08 17:54:32 +0300

    second day of work

--- js/script.js
+++ js/script.js
@@ -6,31 +6,31 @@ for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function () {
         let name = btns[i].getAttribute('data-modal-btn');
         let modal = document.querySelector("[data-modal-window='" + name + "']");
-        modal.style.display = "block";
+        modal.style.display = 'block';
         function openModal(screen) {
             if (screen.matches) {
-                modal.style.animation = "shift-media-right .7s ease";
+                modal.style.animation = 'shift-media-right .7s ease';
             } else {
-                modal.style.animation = "shift-right .7s ease";
+                modal.style.animation = 'shift-right .7s ease';
             }
         }
         let screen = window.matchMedia('(max-width: 768px)');
         openModal(screen);
-        let close = modal.querySelector(".close_modal_window");
+        let close = modal.querySelector('.close_modal_window');
         close.addEventListener('click', function () {
             function closeModal(screen) {
                 if (screen.matches) {
-                    modal.style.animation = "shift-media-left .7s ease";
+                    modal.style.animation = 'shift-media-left .7s ease';
                 } else {
-                    modal.style.animation = "shift-left .7s ease";
+                    modal.style.animation = 'shift-left .7s ease';
                 }
             }
             closeModal(screen);
             setTimeout(function () {
-                modal.style.animationPlayState = "paused";
+                modal.style.animationPlayState = 'paused';
             }, 650);
             setTimeout(function () {
-                modal.style.display = "none";
+                modal.style.display = 'none';
             }, 650);
         });
     });
